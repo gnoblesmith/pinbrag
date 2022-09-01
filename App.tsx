b465aafb1dcfc59fd449eac729d478aedc4b826f
Gabe <gnoblesmith@gmail.com>
2022-08-31 21:10:26 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import TakePicture from './components/TakePicture';
+import ShowPicture from './components/ShowPicture';
+import { S3 } from 'aws-sdk';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef(null);
+  const [imageSource, setImageSource] = useState();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return <View style={styles.container}>
+      <Text style={{ textAlign: 'center' }}>
+        We need your permission to show the camera
+      </Text>
+      <Button onPress={requestPermission} title="grant permission" />
+    </View>
+  }
+
+  const onPictureAvailable = (data) => {
+    console.log(data);
+    setImageSource(data);
+  }
+
+  const onTakeAnother = () => {
+    setImageSource(null);
+  }
+
+  const onSubmit = async () => {
+    if (imageSource != null) {
+      const s3Bucket = new S3({
+        accessKeyId: 'AKIAXVX3XNUFCGHIAU6G',
+        secretAccessKey: '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'
+      });
+
+      let picture = await fetch(imageSource.uri);
+      picture = await picture.blob();
+      const imageData = new File([picture], `my_image.jpg`);
+
+      let uploadParams = {Bucket: 'pinbrag-images-test', Key: 'my_image.jpg', Body: imageData};
+      s3Bucket.upload(uploadParams, (err, data) => {
+        if (err) {
+          console.log("Error", err);
+        }
+        if (data) {
+          console.log("success", data.Location);
+        }
+      });
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      { !imageSource ?
+        <TakePicture onPictureAvailable={onPictureAvailable}/> :
+        <ShowPicture
+          onTakeAnother={onTakeAnother}
+          imageSource={imageSource}
+          onSubmit={onSubmit}
+        />
+      }
     </View>
   );
 }
@@ -13,8 +74,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
